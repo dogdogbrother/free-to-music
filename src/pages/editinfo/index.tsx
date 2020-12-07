f084e23096f9b47f38b7367ec992ca79fc977114
senlin <gougewh@sina.com>
2020-12-07 16:06:59 +0800

    个人信息编辑页面完成

--- src/pages/editinfo/index.tsx
+++ src/pages/editinfo/index.tsx
@@ -1,30 +1,139 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { Wrap } from './style'
 import { PageTitle } from '@/base/style'
-import { InfoItem } from './style'
-import { useSelector } from 'react-redux'
-import { RootState } from '@/models/index'
+import { InfoItem, FileContainer } from './style'
+import { FormOutlined } from '@ant-design/icons'
+import { Input, Radio } from 'antd'
+import { RadioChangeEvent } from 'antd/lib/radio'
+import { fieldMap, genderMap } from '@/config/map'
+import axios from 'axios'
+import { IHttpRes } from '../square/types'
+import { useDispatch } from 'react-redux'
+const USER_INFO = "api/user/info"
 
 const EditInfo = () => {
-  let { nickName, avatar } = useSelector((state: RootState) => state.user)
+  const fileRef = useRef<HTMLInputElement | null>(null)
+  const dispatch = useDispatch()
+  const [ myAvatar, setMyAvatar ] = useState("")
+  const [ formConfig, setFormConfig ] = useState({
+    nickName: "",
+    gender: "2",
+    characterSignature: ""
+  })
+  const [ activeIndex, setActiveIndex ] = useState(-1)
+
+  useEffect(() => {
+    axios.get(USER_INFO).then(res => {
+      const { avatar, nickName, gender, characterSignature } = res.data
+      setMyAvatar(avatar)
+      setFormConfig({
+        nickName,
+        gender,
+        characterSignature
+      })
+    })
+  }, [])
+  
+  function selectImage() {
+    var reader = new FileReader();
+    reader.onload = async function(ev) {
+      if (ev?.target?.result) {
+        const { data }: IHttpRes = await axios.put(USER_INFO, {
+          avatar: ev?.target?.result
+        })
+        setMyAvatar(data.avatar)
+        dispatch({
+          type: 'user/setState',
+          payload: {
+            avatar: data.avatar
+          }
+        })
+      }
+    }
+    if (fileRef.current?.files) {
+      reader.readAsDataURL(fileRef.current?.files[0] as Blob);
+    }
+  }
+  function selectEdit(index: number) {
+    setActiveIndex(index)
+  }
+  function inputBlur(index: number) {
+    const key = index === 0 ? "nickName" :"characterSignature"
+    axios.put(USER_INFO, {[key]: formConfig[key]}).then(res => {
+      setFormConfig({
+        ...formConfig,
+        [key]: res.data[key]
+      })
+      dispatch({
+        type: 'user/setState',
+        payload: {
+          [key]: res.data[key]
+        }
+      })
+      setActiveIndex(-1)
+    })
+  }
+  function radioChange(e: RadioChangeEvent) {
+    axios.put(USER_INFO, {gender: e.target.value}).then(res => {
+      setFormConfig({
+        ...formConfig,
+        gender: res.data.gender
+      })
+      setActiveIndex(-1)
+    })
+  }
   return (
     <Wrap>
       <PageTitle style={{marginBottom: "20px"}}>个人资料</PageTitle>
       <ul>
         <InfoItem>
           <span className="label">头像</span>
-          <div>
-            <img src={avatar} />
-          </div>
+          <FileContainer>
+            <img src={myAvatar} alt="头像" />
+            <input type="file" ref={fileRef} name="fn" onChange={selectImage} accept="image/*"></input>
+          </FileContainer>
         </InfoItem>
-        <InfoItem>
-          <span className="label">用户名</span>
-          <div>
-
-          </div>
-        </InfoItem>
-        <InfoItem>3</InfoItem>
-        <InfoItem>4</InfoItem>
+        {
+          Object.keys(formConfig).map((key, index) => {
+            return (
+              <InfoItem key={index}>
+                <span className="label">{fieldMap[key]}</span>
+                {
+                  activeIndex === index
+                  ?
+                  function inputOrRadio() {
+                    if (fieldMap[key] === '性别') {
+                      return <Radio.Group onChange={radioChange} value={formConfig[key]}>
+                        {Object.keys(genderMap).map(key => (
+                          <Radio key={key} value={key}>{genderMap[key]}</Radio>
+                        ))}
+                      </Radio.Group>
+                    } else {
+                      return <Input 
+                        value={formConfig[key]}
+                        style={{width: "200px"}} 
+                        onChange={(e) => {
+                          formConfig[key] = e.target.value
+                          setFormConfig({
+                            ...formConfig
+                          })
+                        }}
+                        onBlur={() => inputBlur(index)}
+                      />
+                    }
+                  }()
+                  : 
+                  <div>
+                    <span style={{marginRight: "20px"}}>
+                      {key === "gender" ? genderMap[formConfig[key]] : formConfig[key]}
+                    </span>
+                    <FormOutlined className="pointer" onClick={() => selectEdit(index)} />
+                  </div>
+                }
+              </InfoItem>
+            )
+          })
+        }
       </ul>
     </Wrap>
   )
--- src/pages/editinfo/style.ts
+++ src/pages/editinfo/style.ts
@@ -11,13 +11,34 @@ export const InfoItem = styled.li`
   border-bottom: 1px solid #ccc;
   padding: 20px 10px;
   display: flex;
+  line-height: 28px;
   .label {
     font-weight: bold;
     margin-right: 40px;
+    display: flex;
+    flex: column;
+    align-items: center;
+    width: 80px;
   }
+`
+
+export const FileContainer = styled.div`
+  position: relative;
+  z-index: 4;
+  width: 100px;
+  height: 100px;
   img {
-    width: 100px;
-    height: 100px;
-    border-radius: 50px;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+  }
+  input {
+    cursor: pointer;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 5;
+    opacity: 0;
+    left: 0;
   }
 `
